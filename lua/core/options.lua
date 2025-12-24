b5c36e1c235c66ee30136f0a9cfc6afd70fd3ad5
Gabriel Nobre <gabrielsntnobre.dev@gmail.com>
2025-12-24 15:16:51 -0300

    new changes

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,5 @@
 local o = vim.opt
 local g = vim.g
-local cmd = vim.cmd
 
 
 -- Colorscheme
@@ -9,7 +8,7 @@ local cmd = vim.cmd
 o.showmode = false
 o.number = true
 o.relativenumber = true
-o.winbar = "%t %M"
+--o.winbar = "%t %M"
 o.shiftwidth = 4
 o.tabstop = 4
 o.wrap = false
@@ -27,3 +26,34 @@ o.clipboard = "unnamedplus"
 
 g.mapleader = " "
 g.maplocalleader = " "
+
+
+_G.simple_tabline = function()
+    local line = ''
+    local current = vim.api.nvim_get_current_buf()
+    
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_get_option(buf, 'buflisted') then
+            local name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ':t')
+            name = name == '' and '[No Name]' or name
+            
+            -- Ícones simples baseados em extensão (sem dependência externa)
+            local icon = ''
+            if name:match('%.lua$') then icon = ' ' end
+            if name:match('%.py$') then icon = ' ' end
+            if name:match('%.js$') or name:match('%.ts$') then icon = ' ' end
+            if name:match('%.json$') then icon = ' ' end
+            if name:match('%.md$') or name:match('%.txt$') then icon = ' ' end
+            if name:match('%.vim$') or name == 'init.lua' then icon = ' ' end
+            
+            local mod = vim.api.nvim_buf_get_option(buf, 'modified') and ' [+]' or ''
+            
+            line = line .. (buf == current and '%#TabLineSel#' or '%#TabLine#')
+            line = line .. string.format(' %s%s%s ', icon, name, mod)
+        end
+    end
+    
+    return line .. '%#TabLineFill#'
+end
+
+vim.opt.tabline = '%!v:lua.simple_tabline()'
