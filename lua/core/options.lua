a80fe5d0af0864807c2a46e3c028de77701dfb4e
Gabriel Nobre <gabrielsntnobre.dev@gmail.com>
2025-12-28 09:29:35 -0300

    options updated

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -28,32 +28,4 @@ g.mapleader = " "
 g.maplocalleader = " "
 
 
-_G.simple_tabline = function()
-    local line = ''
-    local current = vim.api.nvim_get_current_buf()
-    
-    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-        if vim.api.nvim_buf_get_option(buf, 'buflisted') then
-            local name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ':t')
-            name = name == '' and '[No Name]' or name
-            
-            -- Ícones simples baseados em extensão (sem dependência externa)
-            local icon = ''
-            if name:match('%.lua$') then icon = ' ' end
-            if name:match('%.py$') then icon = ' ' end
-            if name:match('%.js$') or name:match('%.ts$') then icon = ' ' end
-            if name:match('%.json$') then icon = ' ' end
-            if name:match('%.md$') or name:match('%.txt$') then icon = ' ' end
-            if name:match('%.vim$') or name == 'init.lua' then icon = ' ' end
-            
-            local mod = vim.api.nvim_buf_get_option(buf, 'modified') and ' [+]' or ''
-            
-            line = line .. (buf == current and '%#TabLineSel#' or '%#TabLine#')
-            line = line .. string.format(' %s%s%s ', icon, name, mod)
-        end
-    end
-    
-    return line .. '%#TabLineFill#'
-end
 
-vim.opt.tabline = '%!v:lua.simple_tabline()'
